ee7caf07009f993297f2618dfffd7c5c9db6b56c
Laura <laura.jehn@web.de>
2025-05-27 15:19:43 +0200

    modified supabase init

--- Home.py
+++ Home.py
@@ -3,10 +3,14 @@ import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
-load_dotenv()
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = init_connection()
+
 
 st.set_page_config(page_title="Meal Prep Planner", layout="centered", initial_sidebar_state="expanded")
 
